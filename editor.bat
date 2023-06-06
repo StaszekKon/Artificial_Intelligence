51b43906c09e5c9d760400115c1152b4eda96f76
Staszek <stanislaw.konczal@gmail.com>
2023-06-06 09:29:44 +0200

    Zmiana editor.bat

--- editor.bat
+++ editor.bat
@@ -5,14 +5,14 @@ where /q git.exe
 IF ERRORLEVEL 1 (
 	ECHO Warning!!! No git.exe in Path 
 ) ELSE (
-	git status -uno oo.txt I find /i "*"
+	git status -uno oo.txt | find /i "*"
 
 	if not errorlevel 1 (
 		echo !!!!
 		echo Warning !!! oo.txt jest przestarzały!
 		echo !!!
 		choice /m ---------------Czy chcesz zaktualizowac lokalba kopie? ------"
-		if errolevel 1 (
+		if errorlevel 1 (
 			git pull origin
 	)
 )
