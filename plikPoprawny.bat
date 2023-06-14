378d9be56822f6296fb3df61bb31667b56d5366d
Staszek <stanislaw.konczal@gmail.com>
2023-06-14 21:20:56 +0200

    bat

--- plikPoprawny.bat
+++ plikPoprawny.bat
@@ -1,12 +1,12 @@
-@echo off
+@echo on
 
 git remote update > nul 2>&1
 set /a behind=%(git rev-list --count HEAD..origin/master)%
-if %behind%=0 (
+echo %behind%
+if %behind%==0 (
     echo Nie ma zmian do pobrania.
 ) else (
     git pull
     echo Zaktualizowano lokalne repozytorium.
 )
-
 pause
